0ceedf56afcaac8c2a0b1bc724aa316769260b1b
Muhammad Akram Khan <muhammadakramkhan.dev@gmail.com>
2026-01-02 21:21:05 +0500

    Mapped entities to Dtos

--- GameStore.Api/Endpoints/GameEndpoints.cs
+++ GameStore.Api/Endpoints/GameEndpoints.cs
@@ -2,6 +2,7 @@ using System;
 using GameStore.Api.Data;
 using GameStore.Api.Dtos;
 using GameStore.Api.Entities;
+using GameStore.Api.Mapping;
 
 namespace GameStore.Api.Endpoints;
 
@@ -53,37 +54,14 @@ public static class GameEndpoints
         // POST /games
         group.MapPost("/", (CreateGameDto newGame, GameStoreContext dbContext) =>
         {
-            Game game = new()
-            {
-                Name = newGame.Name,
-                Genre = dbContext.Genres.Find(newGame.GenreId),
-                GenreId = newGame.GenreId,
-                Price = newGame.Price,
-                ReleaseDate = newGame.ReleaseDate
 
-            };
-            // GameDto game = new(
-            //     games.Count + 1,
-            //     newGame.Name,
-            //     newGame.Genre,
-            //     newGame.Price,
-            //     newGame.ReleaseDate
-            // );
+            Game game = newGame.ToEntity();
+            game.Genre = dbContext.Genres.Find(newGame.GenreId);
 
             dbContext.Games.Add(game);
             dbContext.SaveChanges();
 
-            GameDto gameDto = new
-            (
-                game.Id,
-                game.Name,
-                game.Genre!.Name,
-                game.Price,
-                game.ReleaseDate
-
-        );
-
-            return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id }, gameDto);
+            return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id }, game.ToDto());
         });
 
         // PUT /games/1
